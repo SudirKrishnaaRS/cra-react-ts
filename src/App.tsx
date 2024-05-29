c76f0b649c1df10ebc9ea023c8e771cfdf2bf1b0
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2024-05-29 19:58:04 +0530

    Lec 182 - Cross component communication

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,13 @@ const App: React.FC = () => {
     },
   ];
 
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );

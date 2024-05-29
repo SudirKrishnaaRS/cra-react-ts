aa8aec8341945296dbcac44565e60fad0da921fd
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2024-05-29 20:09:55 +0530

    Lec 184 - Managing state types

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.type";
 
 const App: React.FC = () => {
-  const todos = [
-    {
-      id: "t1",
-      text: "Finish TS 101",
-    },
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      {
+        id: Math.random().toString(),
+        text: text,
+      },
+    ]);
   };
 
   return (
